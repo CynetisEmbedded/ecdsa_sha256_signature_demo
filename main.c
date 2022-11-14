ca420f8376667f0aa1f5de5529483d75bd2d472a
Dananjaya Ramanayake <dananjaya@cynetis-embedded.com>
2022-11-14 18:54:26 +0100

    refactor to seperate signature generation and verification

--- main.c
+++ main.c
@@ -10,46 +10,19 @@
 YarrowContext yarrowContext;
 
 // Forward declarations
-int generate_random_seed(uint8_t *seed, size_t *randSeedSize);
+error_t trngGetRandomData(uint8_t *data, size_t length);
+error_t signature_generation();
+error_t signature_verification();
 
-// This is the message to be signed.
-unsigned char message[] = {0xF4, 0x5D, 0x55, 0xF3, 0x55, 0x51, 0xE9, 0x75, 0xD6,
-                           0xA8, 0xDC, 0x7E, 0xA9, 0xF4, 0x88, 0x59,
-                           0x39, 0x40, 0xCC, 0x75, 0x69, 0x4A, 0x27, 0x8F,
-                           0x27, 0xE5, 0x78, 0xA1, 0x63, 0xD8, 0x39, 0xB3,
-                           0x40, 0x40, 0x84, 0x18, 0x08, 0xCF, 0x9C, 0x58,
-                           0xC9, 0xB8, 0x72, 0x8B, 0xF5, 0xF9, 0xCE, 0x8E,
-                           0xE8, 0x11, 0xEA, 0x91, 0x71, 0x4F, 0x47, 0xBA,
-                           0xB9, 0x2D, 0x0F, 0x6D, 0x5A, 0x26, 0xFC, 0xFE,
-                           0xEA, 0x6C, 0xD9, 0x3B, 0x91, 0x0C, 0x0A, 0x2C,
-                           0x96, 0x3E, 0x64, 0xEB, 0x18, 0x23, 0xF1, 0x02,
-                           0x75, 0x3D, 0x41, 0xF0, 0x33, 0x59, 0x10, 0xAD,
-                           0x3A, 0x97, 0x71, 0x04, 0xF1, 0xAA, 0xF6, 0xC3,
-                           0x74, 0x27, 0x16, 0xA9, 0x75, 0x5D, 0x11, 0xB8,
-                           0xEE, 0xD6, 0x90, 0x47, 0x7F, 0x44, 0x5C, 0x5D,
-                           0x27, 0x20, 0x8B, 0x2E, 0x28, 0x43, 0x30, 0xFA,
-                           0x3D, 0x30, 0x14, 0x23, 0xFA, 0x7F, 0x2D, 0x08,
-                           0x6E, 0x0A, 0xD0, 0xB8, 0x92, 0xB9, 0xDB, 0x54,
-                           0x4E, 0x45, 0x6D, 0x3F, 0x0D, 0xAB, 0x85, 0xD9,
-                           0x53, 0xC1, 0x2D, 0x34, 0x0A, 0xA8, 0x73, 0xED,
-                           0xA7, 0x27, 0xC8, 0xA6, 0x49, 0xDB, 0x7F, 0xA6,
-                           0x37, 0x40, 0xE2, 0x5E, 0x9A, 0xF1, 0x53, 0x3B,
-                           0x30, 0x7E, 0x61, 0x32, 0x99, 0x93, 0x11, 0x0E,
-                           0x95, 0x19, 0x4E, 0x03, 0x93, 0x99, 0xC3, 0x82,
-                           0x4D, 0x24, 0xC5, 0x1F, 0x22, 0xB2, 0x6B, 0xDE,
-                           0x10, 0x24, 0xCD, 0x39, 0x59, 0x58, 0xA2, 0xDF,
-                           0xEB, 0x48, 0x16, 0xA6, 0xE8, 0xAD, 0xED, 0xB5,
-                           0x0B, 0x1F, 0x6B, 0x56, 0xD0, 0xB3, 0x06, 0x0F,
-                           0xF0, 0xF1, 0xC4, 0xCB, 0x0D, 0x0E, 0x00, 0x1D,
-                           0xD5, 0x9D, 0x73, 0xBE, 0x12};
-
-int generate_random_seed(uint8_t *seed, size_t *randSeedSize)
+error_t trngGetRandomData(uint8_t *data, size_t length)
 {
     // Generatea CSPRNG Seed (32 bytes)
     // https://man7.org/linux/man-pages/man2/getrandom.2.html
     // getrandom() was introduced in version 3.17 of the Linux kernel.
-    *randSeedSize = getrandom(seed, 32, GRND_RANDOM);
-    if (*randSeedSize != 32)
+    ssize_t len;
+    len = getrandom(data, length, GRND_RANDOM);
+
+    if (len != length)
     {
         // Incorrect seed length
         return ERROR_FAILURE;
@@ -58,127 +31,239 @@ int generate_random_seed(uint8_t *seed, size_t *randSeedSize)
     return NO_ERROR;
 }
 
-int main(int argc, char *argv[])
+int init_crypto_rng(uint8_t *seed, size_t length)
 {
+    uint_t i;
+    uint32_t value;
     error_t error;
-    size_t messageLen;
-    uint8_t digest[64];
 
+    //...Generate a random seed (32 bytes) using your hardware here...
+    error = trngGetRandomData(seed, length);
+
+    if (error)
+    {
+        // Debug message
+        printf("Failed to get random data!\r\n");
+    }
+
+    // PRNG initialization
+    error = yarrowInit(&yarrowContext);
+    // Any error to report?
+    if (error)
+    {
+        // Debug message
+        printf("Failed to initialize PRNG!\r\n");
+    }
+
+    // Properly seed the PRNG
+    error = yarrowSeed(&yarrowContext, seed, length);
+    // Any error to report?
+    if (error)
+    {
+        // Debug message
+        printf("Failed to seed PRNG!\r\n");
+    }
+
+    return error;
+}
+
+error_t signature_generation()
+{
+    error_t  error;
     EcDomainParameters params;
     EcPrivateKey privateKey;
-    EcPublicKey publicKey;
     EcdsaSignature signature;
+    uint8_t digest[64];
 
-    Mpi r;
-    Mpi t;
+    //Message to be signed
+    char_t message[] = "test";
 
-    uint8_t randSeed[32];
-    size_t randSeedSize = 0;
+    // ECDSA private key
+    uint8_t d[32] = {
+        0xC9, 0xAF, 0xA9, 0xD8, 0x45, 0xBA, 0x75, 0x16, 0x6B, 0x5C, 0x21, 0x57, 0x67, 0xB1, 0xD6, 0x93,
+        0x4E, 0x50, 0xC3, 0xDB, 0x36, 0xE8, 0x9B, 0x12, 0x7B, 0x8A, 0x62, 0x2B, 0x12, 0x0F, 0x67, 0x21};
 
-    error = NO_ERROR;
-    messageLen = sizeof(message);
+    // Resulting ECDSA signature
+    uint8_t r[32];
+    uint8_t s[32];
 
     // Initialize EC domain parameters
     ecInitDomainParameters(&params);
-    // Load EC domain parameters
-    ecLoadDomainParameters(&params, SECP192R1_CURVE);
     // Initialize ECDSA private key
     ecInitPrivateKey(&privateKey);
-    // Initialize ECDSA public key
-    ecInitPublicKey(&publicKey);
-    // Initialize ECDSA signature (calculated)
+    // Initialize ECDSA signature
     ecdsaInitSignature(&signature);
 
-    // Initialize multiple precision integers
-    mpiInit(&r);
-    mpiInit(&t);
+    // Load EC domain parameters
+    error = ecLoadDomainParameters(&params, SECP256R1_CURVE);
 
-    // start of exception handling block
-    do
+    // Read ECDSA private key
+    if (!error)
     {
-        printf("Initializing CSPRNG...\n");
-        error = generate_random_seed(randSeed, &randSeedSize);
-        if (error)
-        {
-            printf("Error. Random seed initialization failed. (%d)\r\n", error);
-            break;
-        }
+        error = mpiImport(&privateKey.d, d, 32, MPI_FORMAT_BIG_ENDIAN);
+    }
 
-        // Initialize PRNG Algo
-        error = yarrowInit(&yarrowContext);
-        if (error)
-        {
-            printf("Error. CSPRNG initialization failed (%d)\r\n", error);
-            break;
-        }
+    // Digest the message
+    if (!error)
+    {
+        error = sha256Compute(message, strlen(message), digest);
+    }
 
-        // Seed PRNG
-        error = yarrowSeed(&yarrowContext, randSeed, randSeedSize);
-        if (error)
-        {
-            printf("Error. Failed to seed CSPRNG (%d)\r\n", error);
-            break;
-        }
-        printf("Done.\n");
+    // Check status code
+    if (!error)
+    {
+        // Debug message
+        printf("Generating ECDSA signature...\r\n");
 
-        printf("Calculating digest of the message to be signed.\n");
-        // Digest message
-        error = sha256Compute(message, messageLen, digest);
-        if (error)
-        {
-            printf("Error. Failed to digest message.\r\n");
-            break;
-        }
-        printf("Done.\n");
-
-        printf("Generating ECDSA key pair...\n");
-        // ECDSA key pair generation
-        error = ecdsaGenerateKeyPair(YARROW_PRNG_ALGO, &yarrowContext, &params,
-                                     &privateKey, &publicKey);
-
-        if (error)
-        {
-            printf("Error. Failed to generate ECDSA key pair.\r\n");
-            break;
-        }
-        printf("Done.\n");
-
-        printf("Generating ECDSA signature...\n");
         // ECDSA signature generation
         error = ecdsaGenerateSignature(YARROW_PRNG_ALGO, &yarrowContext, &params,
                                        &privateKey, digest, SHA256_DIGEST_SIZE, &signature);
+    }
 
-        if (error)
-        {
-            printf("Error. Failed to generate ECDSA signature.\r\n");
-            break;
-        }
-        printf("Done.\n");
+    // Export the R and S integers of the ECDSA signature
+    if (!error)
+    {
+        error = mpiExport(&signature.r, r, 32, MPI_FORMAT_BIG_ENDIAN);
+    }
 
-        printf("Verifying ECDSA signature...\n");
-        // ECDSA signature verification
-        error = ecdsaVerifySignature(&params, &publicKey, digest,
-                                     SHA256_DIGEST_SIZE, &signature);
-        if (error)
-        {
-            printf("Error. Failed to verify ECDSA signature.\r\n");
-            break;
-        }
-        printf("Done.\n");
-        ecFreeDomainParameters(&params);
+    if (!error)
+    {
+        error = mpiExport(&signature.s, s, 32, MPI_FORMAT_BIG_ENDIAN);
+    }
 
-        printf("ECDSA Signature Generation/Verification Complete.\r\n");
-
-        // end of exception handling block
-    } while (0);
+    if (!error)
+    {
+        printf("ECDSA signature generated.\r\n");
+    }
 
     // Release previously allocated resources
     ecFreeDomainParameters(&params);
     ecFreePrivateKey(&privateKey);
+    ecdsaFreeSignature(&signature);
+
+    // Return status code
+    return error;
+}
+
+error_t signature_verification()
+{
+    error_t error;
+    EcDomainParameters params;
+    EcPublicKey publicKey;
+    EcdsaSignature signature;
+    uint8_t digest[SHA256_DIGEST_SIZE];
+
+    //Message to be signed
+    char_t message[] = "test";
+
+    // ECDSA public key
+    uint8_t qx[32] = {
+        0x60, 0xFE, 0xD4, 0xBA, 0x25, 0x5A, 0x9D, 0x31, 0xC9, 0x61, 0xEB, 0x74, 0xC6, 0x35, 0x6D, 0x68,
+        0xC0, 0x49, 0xB8, 0x92, 0x3B, 0x61, 0xFA, 0x6C, 0xE6, 0x69, 0x62, 0x2E, 0x60, 0xF2, 0x9F, 0xB6};
+
+    uint8_t qy[32] = {
+        0x79, 0x03, 0xFE, 0x10, 0x08, 0xB8, 0xBC, 0x99, 0xA4, 0x1A, 0xE9, 0xE9, 0x56, 0x28, 0xBC, 0x64,
+        0xF2, 0xF1, 0xB2, 0x0C, 0x2D, 0x7E, 0x9F, 0x51, 0x77, 0xA3, 0xC2, 0x94, 0xD4, 0x46, 0x22, 0x99};
+
+    // ECDSA signature
+    uint8_t r[32] = {
+        0xF1, 0xAB, 0xB0, 0x23, 0x51, 0x83, 0x51, 0xCD, 0x71, 0xD8, 0x81, 0x56, 0x7B, 0x1E, 0xA6, 0x63,
+        0xED, 0x3E, 0xFC, 0xF6, 0xC5, 0x13, 0x2B, 0x35, 0x4F, 0x28, 0xD3, 0xB0, 0xB7, 0xD3, 0x83, 0x67};
+
+    uint8_t s[32] = {
+        0x01, 0x9F, 0x41, 0x13, 0x74, 0x2A, 0x2B, 0x14, 0xBD, 0x25, 0x92, 0x6B, 0x49, 0xC6, 0x49, 0x15,
+        0x5F, 0x26, 0x7E, 0x60, 0xD3, 0x81, 0x4B, 0x4C, 0x0C, 0xC8, 0x42, 0x50, 0xE4, 0x6F, 0x00, 0x83};
+
+    // Initialize EC domain parameters
+    ecInitDomainParameters(&params);
+    // Initialize ECDSA public key
+    ecInitPublicKey(&publicKey);
+    // Initialize ECDSA signature
+    ecdsaInitSignature(&signature);
+
+    // Load EC domain parameters
+    error = ecLoadDomainParameters(&params, SECP256R1_CURVE);
+
+    // Read ECDSA public key
+    if (!error)
+    {
+        error = mpiImport(&publicKey.q.x, qx, 32, MPI_FORMAT_BIG_ENDIAN);
+    }
+
+    if (!error)
+    {
+        error = mpiImport(&publicKey.q.y, qy, 32, MPI_FORMAT_BIG_ENDIAN);
+    }
+
+    // Read ECDSA signature
+    if (!error)
+    {
+        error = mpiImport(&signature.r, r, 32, MPI_FORMAT_BIG_ENDIAN);
+    }
+
+    if (!error)
+    {
+        error = mpiImport(&signature.s, s, 32, MPI_FORMAT_BIG_ENDIAN);
+    }
+
+    // Digest the message
+    if (!error)
+    {
+        error = sha256Compute(message, strlen(message), digest);
+    }
+
+    // Check status code
+    if (!error)
+    {
+        // Debug message
+        printf("verifying ECDSA signature...\r\n");
+
+        // ECDSA signature verification
+        error = ecdsaVerifySignature(&params, &publicKey, digest,
+                                     SHA256_DIGEST_SIZE, &signature);
+    }
+
+    // Check status code
+    if (!error)
+    {
+        // Debug message
+        printf("ECDSA signature verified.\r\n");
+    }
+
+    // Release previously allocated resources
+    ecFreeDomainParameters(&params);
     ecFreePublicKey(&publicKey);
     ecdsaFreeSignature(&signature);
-    mpiFree(&r);
-    mpiFree(&t);
+
+    // Return status code
+    return error;
+}
+
+int main(int argc, char *argv[])
+{
+    error_t error;
+    uint8_t seed[32];
+    size_t length = 32;
+
+    error = init_crypto_rng(seed, length);
+    if (error)
+    {
+        printf("Failed to initialize TRNG seed.\r\n");
+    }
+
+    error = signature_generation();
+    if (error)
+    {
+        printf("Failed to generate ECDSA signature.\r\n");
+    }
+
+    printf("\r\n");
+
+    error = signature_verification();
+    if (error)
+    {
+        printf("Failed to verify ECDSA signature.\r\n");
+    }
 
     return NO_ERROR;
 }
